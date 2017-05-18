41548256b5319e2ed9cf2e6f9adc2615a50bd6a7
Charlie Wood <charliemowood@gmail.com>
2017-05-18 11:54:15 +0100

    Use Weapon class within Facade class and utilize in Runner

--- Facade.java
+++ Facade.java
@@ -1,13 +1,20 @@
 public class Facade {
   Holodeck holodeck;
+  Weapon weapon;
 
   public Facade() {
     holodeck = new Holodeck();
+    weapon = new Weapon("Uber Uzi");
   }
 
   public void useHolodeck() {
     System.out.println(holodeck.bootUp());
     System.out.println(holodeck.makeHolodeckySounds());
+  }
+
+  public void grabWeaponAndFire() {
+    System.out.println("You grab your " + weapon.getName());
+    System.out.println(weapon.fire());
   }
 
 }
--- Runner.java
+++ Runner.java
@@ -2,5 +2,6 @@ public class Runner {
   public static void main(String[] args) {
     Facade facade = new Facade();
     facade.useHolodeck();
+    facade.grabWeaponAndFire();
   }
 }
